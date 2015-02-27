8aeee0c3fa1bdfa8e71357e5934bb56f0ac5e29a
Daniel Neal <danielneal2@gmail.com>
2015-02-27 23:02:51 +0000

    Use generated extern

--- dimple/resources/cljsjs/dimple/common/dimple.ext.js
+++ dimple/resources/cljsjs/dimple/common/dimple.ext.js
@@ -1,20 +1,110 @@
-/* @const */
-var dimple = {}
-/* @const */
-dimple.aggregateMethod = {}
-/* @const */
-dimple.axis = {}
-/* @const */
-dimple.chart = {}
-/* @const */
-dimple.color = {}
-/* @const */
-dimple.eventArgs = {}
-/* @const */
-dimple.legend = {}
-/* @const */
-dimple.plot = {}
-/* @const */
-dimple.series = {}
-/* @const */
-dimple.storyboard = {}
+var dimple = {
+    "version": {},
+    "plot": {
+        "area": {
+            "stacked": {},
+            "grouped": {},
+            "supportedAxes": {
+                "0": {},
+                "1": {},
+                "2": {}
+            },
+            "draw": function () {}
+        },
+        "bar": {
+            "stacked": {},
+            "grouped": {},
+            "supportedAxes": {
+                "0": {},
+                "1": {},
+                "2": {}
+            },
+            "draw": function () {}
+        },
+        "bubble": {
+            "stacked": {},
+            "grouped": {},
+            "supportedAxes": {
+                "0": {},
+                "1": {},
+                "2": {},
+                "3": {}
+            },
+            "draw": function () {}
+        },
+        "line": {
+            "stacked": {},
+            "grouped": {},
+            "supportedAxes": {
+                "0": {},
+                "1": {},
+                "2": {}
+            },
+            "draw": function () {}
+        },
+        "pie": {
+            "stacked": {},
+            "grouped": {},
+            "supportedAxes": {
+                "0": {},
+                "1": {},
+                "2": {},
+                "3": {},
+                "4": {}
+            },
+            "draw": function () {}
+        }
+    },
+    "aggregateMethod": {
+        "avg": function () {},
+        "count": function () {},
+        "max": function () {},
+        "min": function () {},
+        "sum": function () {}
+    },
+    "axis": function () {},
+    "chart": function () {},
+    "color": function () {},
+    "eventArgs": function () {},
+    "legend": function () {},
+    "series": function () {},
+    "storyboard": function () {},
+    "_addGradient": function () {},
+    "_arrayIndexCompare": function () {},
+    "_createClass": function () {},
+    "_drawMarkerBacks": function () {},
+    "_drawMarkers": function () {},
+    "_getOrderedList": function () {},
+    "_getSeriesOrder": function () {},
+    "_getSeriesSortPredicate": function () {},
+    "_helpers": {
+        "cx": function () {},
+        "cy": function () {},
+        "r": function () {},
+        "xGap": function () {},
+        "xClusterGap": function () {},
+        "yGap": function () {},
+        "yClusterGap": function () {},
+        "x": function () {},
+        "y": function () {},
+        "width": function () {},
+        "height": function () {},
+        "opacity": function () {},
+        "fill": function () {},
+        "stroke": function () {},
+        "css": function () {}
+    },
+    "_parentHeight": function () {},
+    "_parentWidth": function () {},
+    "_parsePosition": function () {},
+    "_parseXPosition": function () {},
+    "_parseYPosition": function () {},
+    "_postDrawHandling": function () {},
+    "_removeTooltip": function () {},
+    "_rollUp": function () {},
+    "_showBarTooltip": function () {},
+    "_showPointTooltip": function () {},
+    "filterData": function () {},
+    "getUniqueValues": function () {},
+    "newSvg": function () {}
+}
