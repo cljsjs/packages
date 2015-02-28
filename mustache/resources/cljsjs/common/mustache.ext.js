bdab674972c457653912bc3535a1115377e77dbc
Alexander Solovyov <alexander@solovyov.net>
2015-02-28 12:36:06 +0200

    expose full Mustache.js API in externs

--- mustache/resources/cljsjs/common/mustache.ext.js
+++ mustache/resources/cljsjs/common/mustache.ext.js
@@ -1,3 +1,13 @@
 var Mustache = {
-    render: function(templateStr, context) {}
+    name:       "",
+    version:    "",
+    tags:       [],
+    clearCache: function () {},
+    parse:      function () {},
+    render:     function () {},
+    to_html:    function () {},
+    escape:     function () {},
+    Scanner:    function () {},
+    Context:    function () {},
+    Writer:     function () {}
 };
