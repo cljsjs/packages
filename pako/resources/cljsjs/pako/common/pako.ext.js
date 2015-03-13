aac0239df6f06063f9691aea4ee342b001626bc7
hipitihop <anonymous.amphibian@gmail.com>
2015-03-13 19:06:00 +1000

    new package pako 0.2.5 - zlib port to javascript. see: https://github.com/nodeca/pako

---
+++ pako/resources/cljsjs/pako/common/pako.ext.js
@@ -0,0 +1,56 @@
+
+/**
+ * @fileoverview Closure Compiler externs for pako 0.2.5.
+ * @see https://github.com/nodeca/pako
+ * @see http://nodeca.github.io/pako/
+ * @externs
+ */
+
+/**
+ * @type {Object}
+ */
+var pako;
+
+/**
+ * @param {Uint8Array|Array|String} data
+ * @param {Object} options
+ * @return {Uint8Array|Array|String}
+ */
+pako.deflate = function(data, options) {};
+
+
+/**
+ * @param {Uint8Array|Array|String} data
+ * @param {Object} options
+ * @return {Uint8Array|Array|String}
+ */
+pako.deflateRaw = function (data, options) {};
+
+/**
+ * @param {Uint8Array|Array|String} data
+ * @param {Object} options
+ * @return {Uint8Array|Array|String}
+ */
+pako.gzip = function (data, options) {};
+
+/**
+ * @param {Uint8Array|Array|String} data
+ * @param {Object} options
+ * @return {Uint8Array|Array|String}
+ */
+pako.inflate = function (data, options) {};
+
+
+/**
+ * @param {Uint8Array|Array|String} data
+ * @param {Object} options
+ * @return {Uint8Array|Array|String}
+ */
+pako.inflateRaw = function (data, options) {};
+
+/**
+ * @param {Uint8Array|Array|String} data
+ * @param {Object} options
+ * @return {Uint8Array|Array|String}
+ */
+pako.ungzip = function (data, options) {};
