83614b6f37298e7ade8f28ac4c33c9bc6c5b7cdd
Jason Reed <jreed@chef.io>
2015-04-17 19:37:40 -0400

    add peg.js

---
+++ peg/resources/cljsjs/peg/common/peg.ext.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Closure Compiler externs for peg.js 0.8.0
+ * @see http://pegjs.org
+ * @externs
+ */
+
+/* Bare minimum */
+
+var PEG = {};
+
+PEG.buildParser = function(grammar) {};
