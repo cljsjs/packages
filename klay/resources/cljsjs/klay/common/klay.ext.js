18e3d517bc12f38a7a46fe365c7d81034805d8a0
Jeroen Zuijderhoudt <jeroen@moorooboorai.com>
2015-05-12 13:55:35 +0200

    Expose Klayjs 0.3.2.

---
+++ klay/resources/cljsjs/klay/common/klay.ext.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Closure Compiler externs for Kieler KlayJS 0.3.2.
+ * @see http://rtsys.informatik.uni-kiel.de/confluence/display/KIELER/KIELER+Layout
+ * @externs
+ */
+
+// var klayregister;
+// var klaycallback;
+
+var $klay = {};
+
+//var $klay = {
+//   layout: function (obj) {}
+//};
+
+var $klay.layout = function (obj) {};
