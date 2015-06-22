90d2011d6dba4eb4b57ba89d44eca5638f247415
Julien Eluard <julien.eluard@gmail.com>
2015-06-21 21:31:16 -0300

    [dom4] Initial commit.

---
+++ dom4/README.md
@@ -0,0 +1,20 @@
+# cljsjs/dom4
+
+[](dependency)
+```clojure
+[cljsjs/dom4 "1.4.5-0"] ;; latest release
+```
+[](/dependency)
+
+This jar comes with `deps.cljs` as used by the [Foreign Libs][flibs] feature
+of the Clojurescript compiler. After adding the above dependency to your project
+you can require the packaged library like so:
+
+```clojure
+(ns application.core
+  (:require cljsjs.dom4))
+```
+
+`dom4` polyfills DOM4 parentNodes/childNodes entries.
+
+[flibs]: https://github.com/clojure/clojurescript/wiki/Foreign-Dependencies
---
+++ dom4/resources/cljsjs/dom4/common/dom4.ext.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Element} elements
+ */
+Element.prototype.append = function(elements) {};
+
+/**
+ * @param {Element} elements
+ */
+Element.prototype.prepend = function(elements) {};
+
+/**
+ * @param {Element} elements
+ */
+Element.prototype.before = function(elements) {};
+
+/**
+ * @param {Element} elements
+ */
+Element.prototype.after = function(elements) {};
+
+/**
+ * @param {Element} elements
+ */
+Element.prototype.replaceWith = function(elements) {};
+
+/**
+ */
+Element.prototype.remove = function() {};
+
+/**
+ * @param {string}
+ * @return Element
+ */
+Element.prototype.closest = function(selectors) {};
