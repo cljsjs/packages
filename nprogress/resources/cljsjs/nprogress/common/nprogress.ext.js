86ee4ffc977d785ede7fbb49c6a52eadde554af4
dhruvbhatia <dhruv@dhruvbhatia.com>
2015-09-22 15:52:03 +1000

    Added NProgress (http://ricostacruz.com/nprogress/)

---
+++ nprogress/resources/cljsjs/nprogress/common/nprogress.ext.js
@@ -0,0 +1,7 @@
+var nprogress = function(obj) {};
+
+var configure = function(nprogress) {};
+var start = function(nprogress) {};
+var set = function(nprogress) {};
+var inc = function(nprogress) {};
+var done = function(nprogress) {};
