1da59cbfad5ba84a67ecb46d33a5d660d96bb47e
sto <Son.C.To@gmail.com>
2015-10-29 00:50:06 +0000

    on* functions

--- tween/resources/cljsjs/tween/common/tween.ext.js
+++ tween/resources/cljsjs/tween/common/tween.ext.js
@@ -4,6 +4,9 @@ var TWEEN = {
     "Tween": function(){},
     "removeAll": function (){},
     "to": function (){},
+    "onStart": function (){},
+    "onStop": function (){},
+    "onComplete": function (){},
     "onUpdate": function(){},
     "easing": function (){},
     "start": function (){}
