20dc88304cf19316f919d00b7bd2c60a20b12074
sto <Son.C.To@gmail.com>
2015-11-02 15:35:33 -0500

    [tween]  externs for tween.js 16.3.1

---
+++ tween/resources/cljsjs/tween/common/tween.ext.js
@@ -0,0 +1,13 @@
+var TWEEN = {
+    "Easing": {"Exponential":{},
+	       "InOut": {}},
+    "Tween": function(){},
+    "removeAll": function (){},
+    "to": function (){},
+    "onStart": function (){},
+    "onStop": function (){},
+    "onComplete": function (){},
+    "onUpdate": function(){},
+    "easing": function (){},
+    "start": function (){}
+}
