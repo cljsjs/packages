7c335d768df13ee114eb5b546af3a14c4fddc952
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2015-11-06 11:55:21 +0100

    Added first shot at bootstrap-dialog.

---
+++ bootstrap-dialog/README.md
@@ -0,0 +1,19 @@
+# cljsjs/bootstrap-dialog
+[](dependency)
+```clojure
+[cljsjs/bootstrap-dialog "1.34.6-0"] ;; latest release
+```
+[](/dependency)
+
+This jar comes with `deps.cljs` as used by the [Foreign Libs][flibs] feature
+of the Clojurescript compiler. After adding the above dependency to your project
+you can require the packaged library like so:
+
+```clojure
+(ns application.core
+  (:require cljsjs.bootstrap-dialog))
+```
+[flibs]: https://github.com/clojure/clojurescript/wiki/Packaging-Foreign-Dependencies
+
+
+
---
+++ bootstrap-dialog/resources/cljsjs/bootstrap3-dialog/common/bootstrap-dialog.ext.js
@@ -0,0 +1,143 @@
+var Modal;
+var BootstrapDialogModal = function(element, options) {};
+BootstrapDialogModal.getModalVersion = function() {};
+BootstrapDialogModal.ORIGINAL_BODY_PADDING;
+BootstrapDialogModal.METHODS_TO_OVERRIDE = {};
+BootstrapDialogModal.prototype = {
+    constructor: BootstrapDialogModal,
+    getGlobalOpenedDialogs: function() {}
+};
+
+var BootstrapDialog = function(options) {};
+
+BootstrapDialog.BootstrapDialogModal;
+
+BootstrapDialog.NAMESPACE;
+BootstrapDialog.TYPE_DEFAULT;
+BootstrapDialog.TYPE_INFO;
+BootstrapDialog.TYPE_PRIMARY;
+BootstrapDialog.TYPE_SUCCESS;
+BootstrapDialog.TYPE_WARNING;
+BootstrapDialog.TYPE_DANGER;
+BootstrapDialog.DEFAULT_TEXTS = {};
+BootstrapDialog.SIZE_NORMAL;
+BootstrapDialog.SIZE_SMALL;
+BootstrapDialog.SIZE_WIDE;
+BootstrapDialog.SIZE_LARGE;
+BootstrapDialog.BUTTON_SIZES = {};
+BootstrapDialog.ICON_SPINNER;
+
+BootstrapDialog.defaultOptions = {};
+BootstrapDialog.configDefaultOptions = function(options) {};
+BootstrapDialog.dialogs = {};
+BootstrapDialog.openAll = function() {};
+BootstrapDialog.closeAll = function() {};
+BootstrapDialog.moveFocus = function() {};
+BootstrapDialog.METHODS_TO_OVERRIDE = {};
+
+BootstrapDialog.prototype = {
+    constructor: BootstrapDialog,
+    initOptions: function(options) {},
+    holdThisInstance: function() {},
+    initModalStuff: function() {},
+    createModal: function() {},
+    getModal: function() {},
+    setModal: function($modal) {},
+    createModalDialog: function() {},
+    getModalDialog: function() {},
+    setModalDialog: function($modalDialog) {},
+    createModalContent: function() {},
+    getModalContent: function() {},
+    setModalContent: function($modalContent) {},
+    createModalHeader: function() {},
+    getModalHeader: function() {},
+    setModalHeader: function($modalHeader) {},
+    createModalBody: function() {},
+    getModalBody: function() {},
+    setModalBody: function($modalBody) {},
+    createModalFooter: function() {},
+    getModalFooter: function() {},
+    setModalFooter: function($modalFooter) {},
+    createDynamicContent: function(rawContent) {},
+    formatStringContent: function(content) {},
+    setData: function(key, value) {},
+    getData: function(key) {},
+    setId: function(id) {},
+    getId: function() {},
+    getType: function() {},
+    setType: function(type) {},
+    updateType: function() {},
+    getSize: function() {},
+    setSize: function(size) {},
+    updateSize: function() {},
+    getCssClass: function() {},
+    setCssClass: function(cssClass) {},
+    getTitle: function() {},
+    setTitle: function(title) {},
+    updateTitle: function() {},
+    getMessage: function() {},
+    setMessage: function(message) {},
+    updateMessage: function() {},
+    isClosable: function() {},
+    setClosable: function(closable) {},
+    setCloseByBackdrop: function(closeByBackdrop) {},
+    canCloseByBackdrop: function() {},
+    setCloseByKeyboard: function(closeByKeyboard) {},
+    canCloseByKeyboard: function() {},
+    isAnimate: function() {},
+    setAnimate: function(animate) {},
+    updateAnimate: function() {},
+    getSpinicon: function() {},
+    setSpinicon: function(spinicon) {},
+    addButton: function(button) {},
+    addButtons: function(buttons) {},
+    getButtons: function() {},
+    setButtons: function(buttons) {},
+    getButton: function(id) {},
+    getButtonSize: function() {},
+    updateButtons: function() {},
+    isAutodestroy: function() {},
+    setAutodestroy: function(autodestroy) {},
+    getDescription: function() {},
+    setDescription: function(description) {},
+    setTabindex: function(tabindex) {},
+    getTabindex: function() {},
+    updateTabindex: function() {},
+    getDefaultText: function() {},
+    getNamespace: function(name) {},
+    createHeaderContent: function() {},
+    createTitleContent: function() {},
+    createCloseButton: function() {},
+    createBodyContent: function() {},
+    createMessageContent: function() {},
+    createFooterContent: function() {},
+    createFooterButtons: function() {},
+    createButton: function(button) {},
+    enhanceButton: function($button) {},
+    createButtonIcon: function(icon) {},
+    enableButtons: function(enable) {},
+    updateClosable: function() {},
+    onShow: function(onshow) {},
+    onShown: function(onshown) {},
+    onHide: function(onhide) {},
+    onHidden: function(onhidden) {},
+    isRealized: function() {},
+    setRealized: function(realized) {},
+    isOpened: function() {},
+    setOpened: function(opened) {},
+    handleModalEvents: function() {},
+    handleModalBackdropEvent: function() {},
+    isModalEvent: function(event) {},
+    makeModalDraggable: function() {},
+    realize: function() {},
+    open: function() {},
+    close: function() {}
+};
+
+BootstrapDialog.newGuid = function() {};
+BootstrapDialog.show = function(options) {};
+BootstrapDialog.alert = function() {};
+BootstrapDialog.confirm = function() {};
+BootstrapDialog.warning = function(message, callback) {};
+BootstrapDialog.danger = function(message, callback) {};
+BootstrapDialog.success = function(message, callback) {};
