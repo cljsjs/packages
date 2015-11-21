76c88aaab2c3e4aa7269617d523834f77fd0bf7f
dhruvbhatia <dhruv@dhruvbhatia.com>
2015-11-22 07:46:02 +1100

    Updated externs file as per generator tool (http://jmmk.github.io/javascript-externs-generator/)

--- responsive-nav/resources/cljsjs/responsive-nav/common/responsive-nav.ext.js
+++ responsive-nav/resources/cljsjs/responsive-nav/common/responsive-nav.ext.js
@@ -1,7 +1,21 @@
-var responsiveNav = {
+var ResponsiveNav = function () {};
+ResponsiveNav.prototype = {
+    "destroy": function () {},
     "toggle": function () {},
     "open": function () {},
     "close": function () {},
-    "destroy": function () {},
-    "resize": function () {}
-}
+    "resize": function () {},
+    "handleEvent": function () {},
+    "_init": function () {},
+    "_createStyles": function () {},
+    "_removeStyles": function () {},
+    "_createToggle": function () {},
+    "_closeOnNavClick": function () {},
+    "_preventDefault": function () {},
+    "_onTouchStart": function () {},
+    "_onTouchMove": function () {},
+    "_onTouchEnd": function () {},
+    "_onKeyUp": function () {},
+    "_transitions": function () {},
+    "_calcHeight": function () {}
+};
