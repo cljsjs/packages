999604778040bcef753754c8160cfdcf3b31c7e5
Egg Syntax <egg@novonon.com>
2015-12-05 10:15:45 -0500

    [mathbox] Create MathBox package

---
+++ mathbox/resources/cljsjs/mathbox/common/mathbox.ext.js
@@ -0,0 +1,27 @@
+
+// definitely global:
+// getElementById
+
+var ThreeBox = {};
+ThreeBox.preload = function(element, options) {};
+
+var MathBox = {};
+// var mathBox = function(element, options);
+var window.mathBox = function(element, options);
+MathBox.start = function() {};
+MathBox.platonic = function(options) {};
+MathBox.curve = function(options) {};
+MathBox.bezier = function(options) {};
+MathBox.bezierSurface = function(options) {};
+MathBox.grid = function(options) {};
+MathBox.axis = function(options) {};
+MathBox.surface = function(options) {};
+MathBox.viewport = function(options) {};
+MathBox.camera = function(options) {};
+MathBox.animate = function(object, attributes, options) {};
+MathBox.clone = function(selector, options, animate) {};
+MathBox.vector = function(options) {};
+MathBox.remove = function() {};
+
+MathBox.get = function(selector) {};
+MathBox.set = function(selector, options) {};
