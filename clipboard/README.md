686ae6d2e6f64bc6f350d65629c29bfeae3387d6
Andrew Phillips <theasp@gmail.com>
2016-01-16 13:43:42 -0400

    Added clipboard

---
+++ clipboard/README.md
@@ -0,0 +1,23 @@
+# cljsjs/forge
+
+[](dependency)
+```clojure
+[cljsjs/forge "0.6.38-0"] ;; latest release
+```
+[](/dependency)
+
+This jar comes with `deps.cljs` as used by the [Foreign Libs][flibs] feature
+of the Clojurescript compiler. After adding the above dependency to your project
+you can require the packaged library like so:
+
+```clojure
+(ns application.core
+  (:require cljsjs.forge))
+
+(defn do-hash [s]
+  (.toHex (-> (.create js/forge.md.sha512)
+              (.update s)
+              (.digest))))
+```
+
+[flibs]: https://github.com/clojure/clojurescript/wiki/Packaging-Foreign-Dependencies
---
+++ clipboard/resources/cljsjs/clipboard/common/clipboard.ext.js
@@ -0,0 +1,27 @@
+/**
+ * Auto-generated
+ * from https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.5/clipboard.js
+ * by http://www.dotnetwise.com/Code/Externs/index.html
+ *
+ * Used the following in the js console: var foo = new
+ * Clipboard("foo"), then extracted from foo.
+ */
+var Clipboard = {
+    "action": function () {},
+    "target": function () {},
+    "text": function () {},
+    "listener": {
+        "destroy": function () {}
+    },
+    "resolveOptions": function () {},
+    "listenClick": function () {},
+    "onClick": function () {},
+    "defaultAction": function () {},
+    "defaultTarget": function () {},
+    "defaultText": function () {},
+    "destroy": function () {},
+    "on": function () {},
+    "once": function () {},
+    "emit": function () {},
+    "off": function () {}
+}
