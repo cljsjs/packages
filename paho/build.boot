76ab7a2b7507a5b37875a4e2261ed7a330f7cc12
Thomas van der Veen <th.vanderveen@gmail.com>
2016-02-21 17:26:04 +0000

    Got optimised working now...

--- paho/build.boot
+++ paho/build.boot
@@ -1,7 +1,7 @@
 (set-env!
   :resource-paths #{"resources"}
   :dependencies '[[adzerk/bootlaces   "0.1.13" :scope "test"]
-                  [cljsjs/boot-cljsjs "0.5.0" :scope "test"]])
+                  [cljsjs/boot-cljsjs "0.5.1" :scope "test"]])
 
 (require '[adzerk.bootlaces :refer :all]
          '[cljsjs.boot-cljsjs.packaging :refer :all])
@@ -12,7 +12,7 @@
 (task-options!
  pom  {:project     'cljsjs/paho
        :version     +version+
-       :description "a JavaScript MQTT client"
+       :description "A JavaScript MQTT client"
        :url         "http://eclipse.org/paho/"
        :scm         {:url "http://git.eclipse.org/c/paho/org.eclipse.paho.mqtt.javascript.git/"}
        :license     {"EPL" "http://www.eclipse.org/legal/epl-v10.html"}})
@@ -22,7 +22,9 @@
     (download :url "http://download.eclipse.org/paho/1.0/paho.javascript-1.0.1.zip"
               :checksum "1c7ebd961a5d6ed07925fce17f1e9fa7"
               :unzip true)
-    (sift :move {#"^paho.javascript-\d.\d.\d/mqttws31.js$"     "cljsjs/paho/development/mqttws31.inc.js"
-                 #"^paho.javascript-\d.\d.\d/mqttws31-min.js$" "cljsjs/paho/production/mqttws31.min.inc.js"})
+    ;; NB!!!
+    ;; Please note that the minified version of the Paho javascript client
+    ;; doesn't play ball with the Google Closure compiler for some reason
+    (sift :move {#"^paho.javascript-\d.\d.\d/mqttws31.js$"     "cljsjs/paho/development/mqttws31.inc.js" })
     (sift :include #{#"^cljsjs"})
     (deps-cljs :name "cljsjs.paho")))
--- paho/resources/cljsjs/paho/common/paho.ext.js
+++ paho/resources/cljsjs/paho/common/paho.ext.js
@@ -4,34 +4,47 @@
  * @externs
  */
 
-
-
-var Paho = { "Paho": 
+var Paho = { "Paho":
              {"MQTT":
              { "Client":
-               { "connect":  function(connectionOptions) {},
+               { "connect":  function( connectionOptions ) {},
                  "disconnect": function() {},
                  "getTraceLog": function() {},
-                 "send": function(message) {},
+                 "send": function( message ) {},
                  "startTrace": function() {},
                  "stopTrace": function() {},
                  "subscribe": function( filter, subscribeOptions )  {},
-                 "unsubscribe": function( filter, unsubscribeOptions ) {},                
+                 "unsubscribe": function( filter, unsubscribeOptions ) {},
                  "onConnectionLost": function( reasonCode, reasonMessage ) {},
                  "onMessageArrived": function( message ) {},
-                 "onMessageDelivered": function( message ) {} },
+                 "onMessageDelivered": function( message ) {},
+                 "clientId": {},
+                 "host": {} },
+
                "Message":
                  {"onSuccess": function() {},
                   "onFailure": function() {} }}}};
 
-/*
+var destinationName = String;
+var duplicate =  Boolean;
+var qos = Number;
+var retained  =  Boolean;
 
-var Paho = {
-    "Paho": {
-        "MQTT": {
-            "Client": function () {},
-            "Message": function () {}
-        }
-    }
-}
-*/
+var clientId = String;
+var host = String;
+var payloadBytes = Array;
+var payloadString = String;
+var payload = {};
+
+var messageIdentifier = {};
+var payloadMessage   = {};
+var connectStrings = {};
+var topics = {};
+var requestQoS = {};
+
+var cleanSession = {};
+var willMessage = {};
+var isRetained = {};
+var userName = {};
+var password  = {};
+var keepAliveInterval = {};
