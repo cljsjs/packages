3181c6fff34a1cd8537404e324be666c4ed16eb5
Christopher Vermilion <chris@figlyinc.com>
2016-03-02 02:02:35 -0500

    [msgpack-lite] Adjust externs file to use correct global export name
    Now I can use msgpack.encode, etc., from CLJS.

--- msgpack-lite/resources/cljsjs/msgpack-lite/common/msgpack-lite.ext.js
+++ msgpack-lite/resources/cljsjs/msgpack-lite/common/msgpack-lite.ext.js
@@ -1,4 +1,18 @@
-var MsgPackLite = {
-    "encode": function() {},
-    "decode": function() {}
+// msgpack-lite.ext.js
+//
+// Externs file for https://github.com/kawanet/msgpack-lite
+//
+// Very partial implementation so far, just bare bones.
+
+var msgpack = {
+	"encode": function() {},
+	"decode": function() {},
+
+	"codec": {
+		"preset": {
+			"addExtPacker": function() {},
+			"addExtUnpacker": function() {},
+		}
+	}
 };
+
