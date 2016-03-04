63c3c7a4b898eae00ffd162e92671dc99b21f239
Matthew Ratzke <matt@degree9.io>
2016-03-04 13:28:23 -0700

    Added WebComponents-lite and externs
    Seems JsMutationObserver is also in the lite package

---
+++ webcomponents-lite/resources/cljsjs/webcomponents/common/HTMLImports.ext.js
@@ -0,0 +1,12 @@
+var HTMLImports = {
+  "flags": {},
+  "ready": {},
+  "readyTime": {},
+  "IMPORT_LINK_TYPE": {},
+  "useNative": {},
+  "rootDocument": {},
+  "whenReady": function () {},
+  "isIE": {},
+  "addModule": function () {},
+  "initializeModules": function () {}
+};
