b12dc25f1734b330fef1654fd1f256e38f7c2da4
Matthew Ratzke <matt@degree9.io>
2016-03-07 15:47:17 -0700

    added externs

--- scrollify/resources/cljsjs/scrollify/common/scrollify.ext.js
+++ scrollify/resources/cljsjs/scrollify/common/scrollify.ext.js
@@ -1,1 +1,16 @@
-$.prototype.scrollify = function(arg) {};
+var jQuery = {
+  "scrollify": {
+    "move": function () {},
+    "instantMove": function () {},
+    "next": function () {},
+    "previous": function () {},
+    "instantNext": function () {},
+    "instantPrevious": function () {},
+    "destroy": function () {},
+    "update": function () {},
+    "current": function () {},
+    "disable": function () {},
+    "enable": function () {},
+    "isDisabled": function () {}
+  }
+};
