55185d506246efd3e0002ccb05596b0c8b34cf67
Gyorgy Frivolt <fifigyuri@gmail.com>
2016-03-10 14:35:24 +0100

    [victory] created new cljsjs package

---
+++ victory/resources/cljsjs/common/victory.ext.js
@@ -0,0 +1,10 @@
+var Victory = {
+  "VictoryAnimation": function() {},
+  "VictoryAxis": function() {},
+  "VictoryBar": function() {},
+  "VictoryChart": function() {},
+  "VictoryLabel": function() {},
+  "VictoryLine": function() {},
+  "VictoryPie": function() {},
+  "VictoryScatter": function() {}
+}
