e9e10bb0e4f77243e06620ef0a38f0dfe837799e
Levi Tan Ong <levi@byimplication.com>
2016-03-30 20:06:38 +0800

    init textures extern

---
+++ textures/resources/cljsjs/textures/common/textures.ext.js
@@ -0,0 +1,54 @@
+function rand () {}
+function umd () {}
+
+var textures = {
+  circles: function () {},
+  lines: function () {},
+  paths: function () {}
+}
+
+textures.circles.prototype = {
+  heavier: function () {},
+  lighter: function () {},
+  thinner: function () {},
+  thicker: function () {},
+  background: function () {},
+  size: function () {},
+  complement: function () {},
+  radius: function () {},
+  fill: function () {},
+  stroke: function () {},
+  strokeWidth: function () {},
+  id: function () {},
+  url: function () {}
+};
+
+textures.lines.prototype = {
+  background: function () {},
+  shapeRendering: function () {},
+  heavier: function () {},
+  lighter: function () {},
+  thinner: function () {},
+  thicker: function () {},
+  orientation: function () {},
+  size: function () {},
+  stroke: function () {},
+  strokeWidth: function () {},
+  id: function () {},
+  url: function () {}
+};
+
+textures.paths.prototype = {
+  background: function () {},
+  shapeRendering: function () {},
+  heavier: function () {},
+  lighter: function () {},
+  thinner: function () {},
+  thicker: function () {},
+  d: function () {},
+  size: function () {},
+  stroke: function () {},
+  strokeWidth: function () {},
+  id: function () {},
+  url: function () {}
+}
