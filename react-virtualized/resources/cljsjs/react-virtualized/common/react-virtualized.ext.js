269b6250172682a7a0ccff302b2b692b14fb833e
Scott Mitchell <scmitchell@lgscout.com>
2016-05-01 17:00:17 -0400

    Add react-virtualized package

---
+++ react-virtualized/resources/cljsjs/react-virtualized/common/react-virtualized.ext.js
@@ -0,0 +1,14 @@
+var ReactVirtualized = {
+  "ArrowKeyStepper": function () {},
+  "AutoSizer": function () {},
+  "Collection": function () {},
+  "ColumnSizer": function () {},
+  "FlexColumn": function () {},
+  "FlexTable": function () {},
+  "Grid": function () {},
+  "InfiniteLoader": function () {},
+  "ScrollSync": function () {},
+  "SortDirection": {},
+  "SortIndicator": function () {},
+  "VirtualScroll": function () {}
+};
