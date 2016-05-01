93bba5e9c1b5c9ea3ff72763f18bfbb4c3d16948
Scott Mitchell <scmitchell@lgscout.com>
2016-05-01 17:00:17 -0400

    Add react-virtualized package

---
+++ react-virtualized/resources/cljsjs/react-virtualized/common/react-virtualized.ext.js
@@ -0,0 +1,101 @@
+// Generated via http://jmmk.github.io/javascript-externs-generator/
+// Import react via cdn
+// Import react-virtualized
+// Loaded JavaScripts:
+// https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react.js
+// https://rawgit.com/bvaughn/react-virtualized/master/dist/umd/react-virtualized.js
+
+// Reference ns as ReactVirtualized
+var ReactVirtualized = {
+  "ArrowKeyStepper": {
+    "propTypes": {}
+  },
+  "AutoSizer": {
+    "propTypes": {},
+    "defaultProps": {}
+  },
+  "Collection": {
+    "propTypes": {},
+    "defaultProps": {}
+  },
+  "ColumnSizer": {
+    "propTypes": {}
+  },
+  "FlexTable": {
+    "propTypes": {},
+    "defaultProps": {}
+  },
+  "FlexColumn": {
+    "defaultProps": {},
+    "propTypes": {}
+  },
+  "SortDirection": {},
+  "SortIndicator": {
+    "propTypes": {}
+  },
+  "Grid": {
+    "propTypes": {},
+    "defaultProps": {}
+  },
+  "InfiniteLoader": {
+    "propTypes": {},
+    "defaultProps": {}
+  },
+  "ScrollSync": {
+    "propTypes": {}
+  },
+  "VirtualScroll": {
+    "propTypes": {},
+    "defaultProps": {}
+  }
+};
+ReactVirtualized.ArrowKeyStepper.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.AutoSizer.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.Collection.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.ColumnSizer.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.FlexTable.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.FlexColumn.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.Grid.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.InfiniteLoader.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.ScrollSync.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
+ReactVirtualized.VirtualScroll.prototype = {
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
