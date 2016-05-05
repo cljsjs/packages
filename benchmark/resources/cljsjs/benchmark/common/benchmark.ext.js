4a07c10e984afe0b5c16d536f0e2d56295333b66
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-05-05 14:12:58 +0100

    [benchmark] Added v2.1.0 of library

---
+++ benchmark/resources/cljsjs/benchmark/common/benchmark.ext.js
@@ -0,0 +1,96 @@
+// Please use https://github.com/jmmk/javascript-externs-generator
+// to generate the externs
+
+
+var Benchmark = {
+  "options": {},
+  "platform": {},
+  "version": {},
+  "filter": function () {},
+  "formatNumber": function () {},
+  "invoke": function () {},
+  "join": function () {},
+  "runInContext": function () {},
+  "support": {},
+  "each": function () {},
+  "forEach": function () {},
+  "forOwn": function () {},
+  "has": function () {},
+  "indexOf": function () {},
+  "map": function () {},
+  "reduce": function () {},
+  "Deferred": function () {},
+  "Event": function () {},
+  "Suite": {
+    "options": {}
+  }
+};
+Benchmark.prototype = {
+  "count": function () {},
+  "cycles": function () {},
+  "hz": function () {},
+  "compiled": function () {},
+  "error": function () {},
+  "fn": function () {},
+  "aborted": function () {},
+  "running": function () {},
+  "setup": function () {},
+  "teardown": function () {},
+  "stats": function () {},
+  "times": function () {},
+  "abort": function () {},
+  "clone": function () {},
+  "compare": function () {},
+  "emit": function () {},
+  "listeners": function () {},
+  "off": function () {},
+  "on": function () {},
+  "reset": function () {},
+  "run": function () {},
+  "toString": function () {}
+};
+Benchmark.Deferred.prototype = {
+  "benchmark": function () {},
+  "cycles": function () {},
+  "elapsed": function () {},
+  "timeStamp": function () {},
+  "resolve": function () {}
+};
+Benchmark.Event.prototype = {
+  "aborted": function () {},
+  "cancelled": function () {},
+  "currentTarget": function () {},
+  "result": function () {},
+  "target": function () {},
+  "timeStamp": function () {},
+  "type": function () {}
+};
+Benchmark.Suite.prototype = {
+  "length": function () {},
+  "aborted": function () {},
+  "running": function () {},
+  "abort": function () {},
+  "add": function () {},
+  "clone": function () {},
+  "emit": function () {},
+  "filter": function () {},
+  "join": function () {},
+  "listeners": function () {},
+  "off": function () {},
+  "on": function () {},
+  "pop": function () {},
+  "push": function () {},
+  "reset": function () {},
+  "run": function () {},
+  "reverse": function () {},
+  "shift": function () {},
+  "slice": function () {},
+  "sort": function () {},
+  "splice": function () {},
+  "unshift": function () {},
+  "each": function () {},
+  "forEach": function () {},
+  "indexOf": function () {},
+  "map": function () {},
+  "reduce": function () {}
+};
