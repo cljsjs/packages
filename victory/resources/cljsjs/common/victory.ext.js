b192d366138eb66e43e0914dd34145f8cb72e4a7
Estevo U. C. Castro <euccastro@gmail.com>
2016-06-13 00:48:50 +0200

    downgrade to 0.6.1 until VictoryLine gets fixed

--- victory/resources/cljsjs/common/victory.ext.js
+++ victory/resources/cljsjs/common/victory.ext.js
@@ -1,4 +1,11 @@
 // List manually adapted from https://github.com/FormidableLabs/victory/blob/master/src/index.js
+//
+// As of this writing, the latest version is 0.8.0, but VictoryLine won't work
+// for any version after 0.6.1, so we stick with this version until that's
+// fixed.
+//
+// This list should be good for a 0.7.0 release, should you want to make it. For
+// 0.8.0 or later, the VictorySharedEvents entry should be uncommented at least.
 
 var Victory = {
   "VictoryAnimation": function() {},
@@ -11,7 +18,7 @@ var Victory = {
   "VictoryLine": function() {},
   "VictoryPie": function() {},
   "VictoryScatter": function() {},
-  "VictorySharedEvents": function() {},
+//  "VictorySharedEvents": function() {},
   "VictoryStack": function() {},
   "VictoryTransition": function() {}
 }
