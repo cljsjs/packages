02bd57a700540b15573422c2d6b001b2d4d187e3
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-07-04 11:05:20 +0100

    [radium] Initial commit

---
+++ radium/resources/cljsjs/radium/common/radium.ext.js
@@ -0,0 +1,105 @@
+// generated with https://jmmk.github.io/javascript-externs-generator/
+// to regenerate from the tool:
+// first include https://fb.me/react-15.2.0.js
+// then https://cdnjs.cloudflare.com/ajax/libs/radium/0.17.1/radium.js
+// and enter ReactMotion as the javascript object to extern
+
+var Radium = {
+  "Plugins": {
+    "checkProps": function () {},
+    "keyframes": function () {},
+    "mergeStyleArray": function () {},
+    "prefix": function () {},
+    "resolveInteractionStyles": function () {},
+    "resolveMediaQueries": function () {},
+    "visited": function () {}
+  },
+  "Style": {
+    "displayName": {},
+    "propTypes": {
+      "radiumConfig": {
+        "isRequired": function () {}
+      },
+      "rules": {
+        "isRequired": function () {}
+      },
+      "scopeSelector": {
+        "isRequired": function () {}
+      }
+    },
+    "contextTypes": {
+      "_radiumConfig": {
+        "isRequired": function () {}
+      }
+    },
+    "getDefaultProps": {
+      "isReactClassApproved": {}
+    },
+    "defaultProps": {
+      "scopeSelector": {}
+    }
+  },
+  "StyleRoot": {
+    "_isRadiumEnhanced": {},
+    "contextTypes": {
+      "_radiumConfig": {
+        "isRequired": function () {}
+      },
+      "_radiumStyleKeeper": {
+        "isRequired": function () {}
+      }
+    },
+    "childContextTypes": {
+      "_radiumStyleKeeper": {
+        "isRequired": function () {}
+      },
+      "_radiumConfig": {
+        "isRequired": function () {}
+      }
+    },
+    "displayName": {}
+  },
+  "getState": function () {},
+  "keyframes": function () {},
+  "TestMode": {
+    "clearState": function () {},
+    "disable": function () {},
+    "enable": function () {}
+  }
+};
+Radium.Style.prototype = {
+  "constructor": function () {},
+  "__reactAutoBindPairs": function () {},
+  "_buildStyles": function () {},
+  "_buildMediaQueryString": function () {},
+  "render": function () {},
+  "mixins": function () {},
+  "statics": function () {},
+  "propTypes": function () {},
+  "contextTypes": function () {},
+  "childContextTypes": function () {},
+  "getDefaultProps": function () {},
+  "getInitialState": function () {},
+  "getChildContext": function () {},
+  "componentWillMount": function () {},
+  "componentDidMount": function () {},
+  "componentWillReceiveProps": function () {},
+  "shouldComponentUpdate": function () {},
+  "componentWillUpdate": function () {},
+  "componentDidUpdate": function () {},
+  "componentWillUnmount": function () {},
+  "updateComponent": function () {},
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {},
+  "replaceState": function () {},
+  "isMounted": function () {}
+};
+Radium.StyleRoot.prototype = {
+  "componentWillUnmount": function () {},
+  "getChildContext": function () {},
+  "render": function () {},
+  "isReactComponent": function () {},
+  "setState": function () {},
+  "forceUpdate": function () {}
+};
