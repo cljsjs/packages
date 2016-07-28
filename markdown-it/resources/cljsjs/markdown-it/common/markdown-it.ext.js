7888a9110d713df2aa0e8f244134aee06e446c72
António Nuno Monteiro <anmonteiro@gmail.com>
2016-07-29 00:12:37 +0100

    [markdown-it] Add markdown-it library

---
+++ markdown-it/resources/cljsjs/markdown-it/common/markdown-it.ext.js
@@ -0,0 +1,18 @@
+// Generated via http://jmmk.github.io/javascript-externs-generator/
+// Loaded JavaScripts:
+// https://rawgit.com/markdown-it/markdown-it/master/dist/markdown-it.js
+
+// extern the 'markdownit' object
+
+var markdownit = function () {};
+markdownit.prototype = {
+  "set": function () {},
+  "configure": function () {},
+  "enable": function () {},
+  "disable": function () {},
+  "use": function () {},
+  "parse": function () {},
+  "render": function () {},
+  "parseInline": function () {},
+  "renderInline": function () {}
+};
