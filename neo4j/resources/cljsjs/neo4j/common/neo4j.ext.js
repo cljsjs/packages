45f40e8c4051597b7e4e246ab90c147c6921cef4
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-07-29 14:08:21 +0100

    [neo4j] Initial commit

---
+++ neo4j/resources/cljsjs/neo4j/common/neo4j.ext.js
@@ -0,0 +1,43 @@
+// Generated using https://jmmk.github.io/javascript-externs-generator/
+// Export neo4j namespace.
+
+var neo4j = {
+  "v1": {
+    "driver": function () {},
+    "int": function () {},
+    "isInt": function () {},
+    "Neo4jError": {
+      "captureStackTrace": function () {},
+      "stackTraceLimit": {}
+    },
+    "auth": {
+      "basic": function () {}
+    },
+    "types": {
+      "Node": function () {},
+      "Relationship": function () {},
+      "UnboundRelationship": function () {},
+      "PathSegment": function () {},
+      "Path": function () {}
+    },
+    "default": {
+      "driver": function () {},
+      "int": function () {},
+      "isInt": function () {},
+      "Neo4jError": {
+        "captureStackTrace": function () {},
+        "stackTraceLimit": {}
+      },
+      "auth": {
+        "basic": function () {}
+      },
+      "types": {
+        "Node": function () {},
+        "Relationship": function () {},
+        "UnboundRelationship": function () {},
+        "PathSegment": function () {},
+        "Path": function () {}
+      }
+    }
+  }
+};
