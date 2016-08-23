7ae7f89ec020df69422566f56d070ab4e9bdacc4
Vladimir Iakovlev <nvbn.rm@gmail.com>
2016-08-23 17:39:40 +0300

    [stats] Add stats.js - a library for JavaScript performance monitor

---
+++ stats/resources/cljsjs/stats/common/stats.ext.js
@@ -0,0 +1,15 @@
+var Stats = {
+  "Panel": {
+    "dom": {},
+    "update": function () {}
+  },
+  "REVISION": {},
+  "dom": {},
+  "addPanel": function () {},
+  "showPanel": function () {},
+  "begin": function () {},
+  "end": function () {},
+  "update": function () {},
+  "domElement": {},
+  "setMode": function () {}
+};
