bc3a64e1dd26cbf085ad2275c8674addd0d3aa21
Daniel Carleton <dacc@uw.edu>
2016-09-26 12:39:42 -0700

    rebass 0.3.2

---
+++ rebass/project.clj
@@ -0,0 +1,12 @@
+(defproject
+  cljsjs/rebass
+  "0.3.2-0"
+  :dependencies
+  [[cljsjs/boot-cljsjs "0.5.1" :scope "test"]
+   [boot/core "2.6.0"]
+   [boot/base "2.6.0"]
+   [boot/aether "2.6.0"]
+   [boot/pod "2.6.0"]
+   [org.clojure/clojure "1.9.0-alpha10"]]
+  :source-paths
+  ["resources" ""])
