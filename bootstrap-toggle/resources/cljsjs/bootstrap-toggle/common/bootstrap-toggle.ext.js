084fc9aa1c440e0f40ebf304e414249402368f42
oakes <zsoakes@gmail.com>
2016-10-08 09:06:42 -0400

    Fix bootstrap-toggle externs

--- bootstrap-toggle/resources/cljsjs/bootstrap-toggle/common/bootstrap-toggle.ext.js
+++ bootstrap-toggle/resources/cljsjs/bootstrap-toggle/common/bootstrap-toggle.ext.js
@@ -1,13 +1,5 @@
-var Toggle = function () {};
-Toggle.prototype.defaults = function () {};
-Toggle.prototype.render = function () {};
-Toggle.prototype.toggle = function () {};
-Toggle.prototype.on = function () {};
-Toggle.prototype.off = function () {};
-Toggle.prototype.enable = function () {};
-Toggle.prototype.disable = function () {};
-Toggle.prototype.update = function () {};
-Toggle.prototype.trigger = function () {};
-Toggle.prototype.destroy = function () {};
+/**
+ * @return {jQuery}
+ */
+jQuery.prototype.bootstrapToggle = function() {};
 
-var Plugin = function () {};
