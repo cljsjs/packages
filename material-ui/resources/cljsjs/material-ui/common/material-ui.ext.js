1d4b8edf22d68f85c7568f6ef5800809c06c4e42
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2016-10-17 01:13:37 +0200

    [material-ui] Fix missing extern for autocomplete filter options

--- material-ui/resources/cljsjs/material-ui/common/material-ui.ext.js
+++ material-ui/resources/cljsjs/material-ui/common/material-ui.ext.js
@@ -1,18 +1,24 @@
 var MaterialUI = {
-
-}
+    AutoComplete : {
+        noFilter: {},
+        defaultFilter: {},
+        caseInsensitiveFilter: {},
+        levenshteinDistanceFilter: {},
+        fuzzyFilter: {}
+    }
+};
 
 var MaterialUIStyles = {
     MuiThemeProvider : function() {},
     getMuiTheme : function() {},
     themeManager: {
-          getMuiTheme: function() {},
-          modifyRawThemeFontFamily: function() {},
-          modifyRawThemePalette: function() {},
-          modifyRawThemeSpacing: function() {}
+        getMuiTheme: function() {},
+        modifyRawThemeFontFamily: function() {},
+        modifyRawThemePalette: function() {},
+        modifyRawThemeSpacing: function() {}
     },
     transitions : {
         create : function() {},
         easeOut  : function() {}
     }
-}
+};
