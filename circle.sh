30da12e91e1f02ebcf766207f04ed82e90045c28
Juho Teperi <juho.teperi@iki.fi>
2016-10-31 00:49:18 +0200

    Test extern files before deploy

--- circle.sh
+++ circle.sh
@@ -5,6 +5,12 @@ set -e
 # Node 0 runs the build or deploy
 # Node 1 runs extern tests
 
+if [[ $CIRCLE_NODE_TOTAL < 2 ]] || [[ $CIRCLE_NODE_INDEX == 1 ]]; then
+    echo "Validate extern files"
+    npm install
+    ./test-extern-files.sh
+fi
+
 if [[ $CIRCLE_NODE_TOTAL < 2 ]] || [[ $CIRCLE_NODE_INDEX == 0 ]]; then
     curl -L https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh -o ~/bin/boot
     chmod +x ~/bin/boot
@@ -29,10 +35,4 @@ if [[ $CIRCLE_NODE_TOTAL < 2 ]] || [[ $CIRCLE_NODE_INDEX == 0 ]]; then
         echo "Test changed packages"
         ./test-changed.sh
     fi
-fi
-
-if [[ $CIRCLE_NODE_TOTAL < 2 ]] || [[ $CIRCLE_NODE_INDEX == 1 ]]; then
-    echo "Validate extern files"
-    npm install
-    ./test-extern-files.sh
 fi
