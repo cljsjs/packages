7775ecf15caeba3cf636d8b752bcf793dc649ef3
madvas <matus.lestan@gmail.com>
2016-11-09 16:49:08 +0100

    [material-ui] Added withWidth

--- material-ui/resources/main.js
+++ material-ui/resources/main.js
@@ -4,10 +4,13 @@
   var injectTapEventPlugin = require("react-tap-event-plugin");
   var materialUI = require('./build/index.js');
   var materialUIStyles = require('./build/styles/index.js');
+  var withWidth = require('./build/utils/withWidth.js');
 
   window["React"] = React;
   window["MaterialUI"] = materialUI;
   window["MaterialUIStyles"] = materialUIStyles;
-
+  window["MaterialUIUtils"] = {
+    withWidth : withWidth
+  };
   injectTapEventPlugin();
 })();
