f92acf3096f1bbb222c3eaa0cda1b06385f2b578
Denis Treskunov <denis.treskunov@gmail.com>
2017-03-26 19:09:26 -0700

    Initial commit of tableauwdc cljsjs library
    Ran into https://github.com/cljsjs/boot-cljsjs/issues/48 which results
    in broken JAR when built on Windows, but should work on a sane(r) platform.

---
+++ tableauwdc/resources/cljsjs/common/tableauwdc.ext.js
@@ -0,0 +1,14 @@
+var tableau = {
+  "phase": {},
+  "connectionData": {},
+  "connectionName": {},
+  "username": {},
+  "password": {},
+  "authType": {},
+  "version": {},
+  "log": function() {},
+  "reportProgress": function() {},
+  "makeConnector": function() {},
+  "registerConnector": function() {},
+  "submit": function() {}
+};
