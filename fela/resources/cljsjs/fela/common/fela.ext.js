07dc4ffac15369c518e024d68c22849b9baae4f3
Christopher Howard <derhowiedesigns@gmail.com>
2017-03-29 10:13:03 -0400

    Create package for fela.js

---
+++ fela/resources/cljsjs/fela/common/fela.ext.js
@@ -0,0 +1,13 @@
+var Fela = {};
+
+Fela.createRenderer = function(){};
+Fela.createRenderer.renderRule = function(){};
+Fela.createRenderer.renderKeyframe = function(){};
+Fela.createRenderer.renderFont = function(){};
+Fela.createRenderer.renderStatic = function(){};
+Fela.createRenderer.renderToString = function(){};
+Fela.createRenderer.subscribe = function(){};
+Fela.createRenderer.clear = function(){};
+
+Fela.combineRules = function(){};
+Fela.enhance = function(){};
