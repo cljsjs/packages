85f00d5f60474a6a99a84a629abc0a81ca79b8f6
madvas <matus.lestan@gmail.com>
2017-04-08 16:41:16 +0700

    [react-flexbox-grid] Update to version 1.0.0

--- react-flexbox-grid/resources/cljsjs/react-flexbox-grid/common/react-flexbox-grid.ext.js
+++ react-flexbox-grid/resources/cljsjs/react-flexbox-grid/common/react-flexbox-grid.ext.js
@@ -2,4 +2,6 @@ var ReactFlexboxGrid = {
     "Row": function () {},
     "Col": function () {},
     "Grid": function () {},
+    "getRowProps": function () {},
+    "getColumnProps": function () {}
 }
