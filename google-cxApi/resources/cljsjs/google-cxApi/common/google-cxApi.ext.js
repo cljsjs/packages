a607710b99ec1230350c6a27ef498750d19bfc5e
Juho Teperi <teeepeli@gmail.com>
2017-09-06 17:54:56 +0300

    Update google-cxApi.ext.js

--- google-cxApi/resources/cljsjs/google-cxApi/common/google-cxApi.ext.js
+++ google-cxApi/resources/cljsjs/google-cxApi/common/google-cxApi.ext.js
@@ -1,27 +1,3 @@
-/***************************************************************************
- * Retrieved on 2015-09-22 from
- * https://raw.githubusercontent.com/google/closure-compiler/master/contrib/externs/google_universal_analytics_api.js
- *
- * The latest release as of 2015-09-22 is 2015-04-13 according to
- * https://developers.google.com/analytics/devguides/collection/analyticsjs/changelog
- ***************************************************************************/
-
-/*
- * Copyright 2015 The Closure Compiler Authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /**
  * @fileoverview Externs for the Google Content Experiments API (cxApi.js).
  *
@@ -45,7 +21,6 @@
  * @externs
  */
  
-
 function cxApi() {}
 
 cxApi.ORIGINAL_VARIATION;
