d5ebe498d4a4f79fc8d7a00c321ba0218ac3c822
Juho Teperi <juho.teperi@iki.fi>
2017-09-28 22:12:19 +0300

    Add missing file and remove old files

--- react_15/resources/react-dom-deps.cljs
+++
@@ -1,6 +0,0 @@
-{:foreign-libs [{:file "cljsjs/react-dom/development/react-dom.inc.js"
-                 :provides ["cljsjs.react.dom" "react-dom"]
-                 :requires ["react"]
-                 :global-exports {react-dom ReactDOM}
-                 :file-min "cljsjs/react-dom/production/react-dom.min.inc.js"}]
- :externs ["cljsjs/react-dom/common/react-dom.ext.js"]}
