2ec83db55c538e961bd6f73c25f06dde86d765eb
James <jmborden@gmail.com>
2017-10-08 18:25:54 -0400

    initial attempt to add howler to cljsjs

---
+++ howler/resources/cljsjs/howler/common/howler.ext.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview Externs for Howler version 2.0.5
+ */
+
+/**
+ * @constructor
+ */
+var Howl = function(o) {};
+
+Howl.prototype.init = function(o) {};
+Howl.prototype.load = function() {};
+Howl.prototype.play = function(sprite, internal) {};
+Howl.prototype.pause = function(id) {};
+Howl.prototype.stop = function(id, internal) {};
+Howl.prototype.mute = function(muted, id) {};
+Howl.prototype.volume = function() {};
+Howl.prototype.fade = function(from, to, len, id) {};
+Howl.prototype.loop = function() {};
+Howl.prototype.rate = function() {};
+Howl.prototype.seek = function() {};
+Howl.prototype.playing = function(id) {};
+Howl.prototype.duration = function(id) {};
+Howl.prototype.state = function() {};
+Howl.prototype.unload = function() {};
+Howl.prototype.on = function(event, fn, id, once) {};
+Howl.prototype.off = function(event, fn, id) {};
+Howl.prototype.once = function(event, fn, id) {};
+
+
