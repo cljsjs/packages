a11b681f918c813a59fd1784ea91f868e0c0cd8b
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2017-10-27 11:19:15 +0100

    [moment-timezone] Update externs

--- moment-timezone/resources/cljsjs/common/moment-timezone.ext.js
+++ moment-timezone/resources/cljsjs/common/moment-timezone.ext.js
@@ -1,23 +1,33 @@
-"use strict";
 
-/**
- * @fileoverview Externs for moment-timezone.js 0.5.10
- * @externs
- */
+var moment = {};
+moment.prototype.tz = function() {};
 
-/**
- * @interface
- */
-function Moment() {};
+moment.prototype.tz.prototype = {
+    "Zone": function () {},
+    "_links": {},
+    "_names": {},
+    "_zones": {},
+    "add": function () {},
+    "dataVersion": {},
+    "guess": function () {},
+    "link": function () {},
+    "load": function () {},
+    "moveAmbiguousForward": {},
+    "moveInvalidForward": {},
+    "names": function () {},
+    "needsOffset": function () {},
+    "setDefault": function () {},
+    "unpack": function () {},
+    "unpackBase60": function () {},
+    "version": {},
+    "zone": function () {},
+    "zoneExists": function () {}
+};
 
-/**
- * @nosideeffects
- * @type {(function():!Moment|function(!Date):!Moment|function(!number):!Moment|function(!Array.<!number>):!Moment|function(!string, !(string|Array.<!string>)=):!Moment|function(!Moment):!Moment)}
- */
-function moment() {};
-
-/**
- * @since 0.0.1
- * @type {(function():!Moment|function(!Date, !string):!Moment|function(!number, !string):!Moment|function(!Array.<!number>, !string):!Moment|function(!string, !(string|Array.<!string>)=):!Moment|function(!Moment, !string):!Moment)}
- */
-Moment.prototype.tz = function() {};
+moment.prototype.tz.Zone.prototype = {
+  "_index": function () {},
+  "_set": function () {},
+  "abbr": function () {},
+  "offset": function () {},
+  "parse": function () {}
+};
