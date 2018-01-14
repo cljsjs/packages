8e2d58f6899696c9263f288b47d4c297a1801b0d
Frozenlock <frozenlock@gmail.com>
2018-01-14 17:57:33 -0500

    add 'react-beautiful-dnd'

---
+++ react-beautiful-dnd/build.boot
@@ -0,0 +1,31 @@
+(def +lib-version+ "4.0.0")
+(def +version+ (str +lib-version+ "-0"))
+
+(set-env!
+  :resource-paths #{"resources"}
+  :dependencies '[[cljsjs/boot-cljsjs "0.10.0" :scope "test"]
+                  [cljsjs/react "16.2.0-3"]])
+
+(require '[cljsjs.boot-cljsjs.packaging :refer :all])
+
+(task-options!
+ pom  {:project     'cljsjs/react-beautiful-dnd
+       :version     +version+
+       :description "Beautiful, accessible drag and drop for lists with React.js"
+       :url         "https://github.com/atlassian/react-beautiful-dnd"
+       :scm         {:url "https://github.com/cljsjs/packages"}
+       :license     {"MIT" "http://opensource.org/licenses/MIT"}})
+
+(deftask package []
+  (comp
+   (download :url (format "https://unpkg.com/react-beautiful-dnd@%s/dist/react-beautiful-dnd.js" +lib-version+)
+             :target "cljsjs/react-beautiful-dnd/development/react-beautiful-dnd.inc.js")
+   (download :url (format "https://unpkg.com/react-beautiful-dnd@%s/dist/react-beautiful-dnd.min.js" +lib-version+)
+             :target "cljsjs/react-beautiful-dnd/production/react-beautiful-dnd.min.inc.js")
+
+   (sift :include #{#"^cljsjs"})
+   (deps-cljs :name "cljsjs.react-beautiful-dnd"
+              :requires ["cljsjs.react"])
+   (pom)
+   (jar)
+   (validate-checksums)))
---
+++ react-beautiful-dnd/resources/cljsjs/react-beautiful-dnd/common/react-beautiful-dnd.ext.js
@@ -0,0 +1,154 @@
+// Generated via http://jmmk.github.io/javascript-externs-generator/
+
+// Loaded JavaScripts:
+// https://github.com/facebook/react/releases/download/v16.0.0/react.production.min.js
+// https://unpkg.com/react-beautiful-dnd@4.0.0/dist/react-beautiful-dnd.min.js
+
+// Reference ns as ReactBeautifulDnd
+
+var ReactBeautifulDnd = {
+  "DragDropContext": {
+    "childContextTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-can-lift": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-dimension-marshal": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-store": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-style-context": {
+        "isRequired": function () {}
+      }
+    }
+  },
+  "Draggable": {
+    "WrappedComponent": {
+      "contextTypes": {
+        "private-react-beautiful-dnd-key-do-not-use-droppable-id": {
+          "isRequired": function () {}
+        },
+        "private-react-beautiful-dnd-key-do-not-use-style-context": {
+          "isRequired": function () {}
+        }
+      },
+      "defaultProps": {
+        "disableInteractiveElementBlocking": {},
+        "isDragDisabled": {}
+      }
+    },
+    "childContextTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-storeSubscription": {
+        "isRequired": function () {}
+      }
+    },
+    "contextTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-store": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-storeSubscription": {
+        "isRequired": function () {}
+      }
+    },
+    "displayName": {},
+    "propTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-store": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-storeSubscription": {
+        "isRequired": function () {}
+      }
+    }
+  },
+  "Droppable": {
+    "WrappedComponent": {
+      "childContextTypes": {
+        "private-react-beautiful-dnd-key-do-not-use-droppable-id": {
+          "isRequired": function () {}
+        }
+      },
+      "defaultProps": {
+        "direction": {},
+        "ignoreContainerClipping": {},
+        "isDropDisabled": {},
+        "type": {}
+      }
+    },
+    "childContextTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-storeSubscription": {
+        "isRequired": function () {}
+      }
+    },
+    "contextTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-store": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-storeSubscription": {
+        "isRequired": function () {}
+      }
+    },
+    "displayName": {},
+    "propTypes": {
+      "private-react-beautiful-dnd-key-do-not-use-store": {
+        "isRequired": function () {}
+      },
+      "private-react-beautiful-dnd-key-do-not-use-storeSubscription": {
+        "isRequired": function () {}
+      }
+    }
+  }
+};
+ReactBeautifulDnd.DragDropContext.prototype = {
+  "forceUpdate": function () {},
+  "isReactComponent": function () {},
+  "setState": function () {}
+};
+ReactBeautifulDnd.Draggable.prototype = {
+  "addExtraProps": function () {},
+  "componentDidMount": function () {},
+  "componentWillReceiveProps": function () {},
+  "componentWillUnmount": function () {},
+  "forceUpdate": function () {},
+  "getChildContext": function () {},
+  "getWrappedInstance": function () {},
+  "initSelector": function () {},
+  "initSubscription": function () {},
+  "isReactComponent": function () {},
+  "isSubscribed": function () {},
+  "notifyNestedSubsOnComponentDidUpdate": function () {},
+  "onStateChange": function () {},
+  "render": function () {},
+  "setState": function () {},
+  "setWrappedInstance": function () {},
+  "shouldComponentUpdate": function () {}
+};
+ReactBeautifulDnd.Draggable.WrappedComponent.prototype = {
+  "forceUpdate": function () {},
+  "isReactComponent": function () {},
+  "setState": function () {}
+};
+ReactBeautifulDnd.Droppable.prototype = {
+  "addExtraProps": function () {},
+  "componentDidMount": function () {},
+  "componentWillReceiveProps": function () {},
+  "componentWillUnmount": function () {},
+  "forceUpdate": function () {},
+  "getChildContext": function () {},
+  "getWrappedInstance": function () {},
+  "initSelector": function () {},
+  "initSubscription": function () {},
+  "isReactComponent": function () {},
+  "isSubscribed": function () {},
+  "notifyNestedSubsOnComponentDidUpdate": function () {},
+  "onStateChange": function () {},
+  "render": function () {},
+  "setState": function () {},
+  "setWrappedInstance": function () {},
+  "shouldComponentUpdate": function () {}
+};
+ReactBeautifulDnd.Droppable.WrappedComponent.prototype = {
+  "forceUpdate": function () {},
+  "isReactComponent": function () {},
+  "setState": function () {}
+};
