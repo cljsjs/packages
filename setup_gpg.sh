c75654bf0a33be9919c031c6d799a25a7a1caf63
Juho Teperi <juho.teperi@iki.fi>
2018-03-28 20:18:54 +0300

    Maybe fix gpg config?

--- setup_gpg.sh
+++ setup_gpg.sh
@@ -6,9 +6,9 @@ set -e
 if [[ $CIRCLE_BRANCH == "master" ]]; then
     mkdir -p ~/.gnupg
     chmod 700 ~/.gnupg
-    echo -e "default-key cljsjs.robot@gmail.com\npassphrase $GPG_PASSPHRASE" >> ~/.gnupg/gpg.conf
+    echo -e "batch\nyes\ndefault-key cljsjs.robot@gmail.com\npassphrase $GPG_PASSPHRASE" >> ~/.gnupg/gpg.conf
     # CircleCI doesn't support newlines in vars so newlines are coded as "|"
     echo -e "$GPG_KEY" | tr "|" "\n" > gpg.key
-    gpg --batch --yes --allow-secret-key-import --import gpg.key
+    gpg --allow-secret-key-import --import gpg.key
     rm gpg.key
 fi
