012eaac0ba69fd5bf30897eaf53b014f8efd1385
Juho Teperi <juho.teperi@iki.fi>
2018-03-28 19:54:00 +0300

    Fix gpg config?

--- setup_gpg.sh
+++ setup_gpg.sh
@@ -9,6 +9,6 @@ if [[ $CIRCLE_BRANCH == "master" ]]; then
     echo -e "default-key cljsjs.robot@gmail.com\npassphrase $GPG_PASSPHRASE" >> ~/.gnupg/gpg.conf
     # CircleCI doesn't support newlines in vars so newlines are coded as "|"
     echo -e "$GPG_KEY" | tr "|" "\n" > gpg.key
-    gpg --allow-secret-key-import --import gpg.key
+    GIG_AGENT_INFO="" gpg --allow-secret-key-import --import gpg.key
     rm gpg.key
 fi
