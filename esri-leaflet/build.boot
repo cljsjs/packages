e74ce84d3b9fd3b4d09fa88f5676a227d29280f2
Kevin Stehn <kevin.stehn@data42.de>
2019-05-28 18:16:05 +0200

    Add esri-leaflet in version 2.2.4

---
+++ esri-leaflet/build.boot
@@ -0,0 +1,30 @@
+(set-env!
+  :resource-paths #{"resources"}
+  :dependencies '[[cljsjs/boot-cljsjs "0.10.3"  :scope "test"]
+                  [cljsjs/leaflet "1.4.0-0"]])
+
+(require '[cljsjs.boot-cljsjs.packaging :refer :all])
+
+(def +lib-version+ "2.2.4")
+(def +version+ (str +lib-version+ "-0"))
+
+(task-options!
+ pom  {:project     'cljsjs/esri-leaflet
+       :version     +version+
+       :description (str "A lightweight set of tools for working with ArcGIS services in Leaflet.")
+       :url         "https://github.com/Esri/esri-leaflet/"
+       :scm         {:url "https://github.com/cljsjs/packages"}
+       :license     {"Apache License 2.0" "https://opensource.org/licenses/Apache-2.0"}})
+
+(deftask package []
+  (comp
+   (download :url (format "https://unpkg.com/esri-leaflet@%s/dist/esri-leaflet-debug.js" +lib-version+)
+             :target "cljsjs/esri-leaflet/development/esri-leaflet.inc.js")
+   (download :url (format "https://unpkg.com/esri-leaflet@%s/dist/esri-leaflet.js" +lib-version+)
+             :target "cljsjs/esri-leaflet/production/esri-leaflet.min.inc.js")
+   (deps-cljs :name "cljsjs.esri-leaflet"
+              :requires ["cljsjs.leaflet"]
+              )
+   (pom)
+   (jar)
+   (validate)))
---
+++ esri-leaflet/resources/cljsjs/esri-leaflet/common/esri-leaflet.ext.js
@@ -0,0 +1,68 @@
+var L = {
+  "esri": function () {}
+};
+
+L.esri.prototype = {
+  "VERSION": {},
+  "Support": {
+    "cors": {},
+    "pointerEvents": {}
+  },
+  "options": {
+    "attributionWidthOffset": {}
+  },
+  "Util": {
+    "warn": function () {},
+    "cleanUrl": function () {},
+    "getUrlParams": function () {},
+    "isArcgisOnline": function () {},
+    "geojsonTypeToArcGIS": function () {},
+    "responseToFeatureCollection": function () {},
+    "geojsonToArcGIS": function () {},
+    "arcgisToGeoJSON": function () {},
+    "boundsToExtent": function () {},
+    "extentToBounds": function () {},
+    "calcAttributionWidth": function () {},
+    "setEsriAttribution": function () {},
+    "_setGeometry": function () {},
+    "_getAttributionData": function () {},
+    "_updateMapAttribution": function () {},
+    "_findIdAttributeFromFeature": function () {},
+    "_findIdAttributeFromResponse": function () {}
+  },
+  "get": function () {},
+  "post": function () {},
+  "request": function () {},
+  "Task": function () {},
+  "task": function () {},
+  "Query": function () {},
+  "query": function () {},
+  "Find": function () {},
+  "find": function () {},
+  "Identify": function () {},
+  "identify": function () {},
+  "IdentifyFeatures": function () {},
+  "identifyFeatures": function () {},
+  "IdentifyImage": function () {},
+  "identifyImage": function () {},
+  "Service": function () {},
+  "service": function () {},
+  "MapService": function () {},
+  "mapService": function () {},
+  "ImageService": function () {},
+  "imageService": function () {},
+  "FeatureLayerService": function () {},
+  "featureLayerService": function () {},
+  "BasemapLayer": function () {},
+  "basemapLayer": function () {},
+  "TiledMapLayer": function () {},
+  "tiledMapLayer": function () {},
+  "RasterLayer": function () {},
+  "ImageMapLayer": function () {},
+  "imageMapLayer": function () {},
+  "DynamicMapLayer": function () {},
+  "dynamicMapLayer": function () {},
+  "FeatureManager": function () {},
+  "FeatureLayer": function () {},
+  "featureLayer": function () {},
+};
