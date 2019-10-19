bd3baeb02ce4233ae7abd920895ba1b69d51a1ce
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-10-19 12:42:35 +0300

    [material-ui-pickers] Update to v3.2.7
    Update library to v3.2.7 and remove bundled material-ui core
    in favor of a dependency to cljsjs.material-ui. Bundling the
    core library causes it to be loaded twice on any project that
    also depends on cljsjs.material-ui.

--- material-ui-pickers/resources/webpack.config.js
+++ material-ui-pickers/resources/webpack.config.js
@@ -5,8 +5,20 @@ module.exports = {
   output: {
     filename: '[name].inc.js'
   },
-  externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM'
-  }
+  externals: [
+    function(context, request, callback) {
+      const re = /^@material-ui\/core\/(.*)$/;
+      if (re.test(request)) {
+        const [_, name] = re.exec(request);
+        const external = name === 'styles' ? 'MaterialUIStyles' : `MaterialUI.${name}`;
+        return callback(null, `root ${external}`);
+      }
+      callback();
+    },
+    {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      '@material-ui/styles': 'MaterialUIStyles'
+    }
+  ]
 };
